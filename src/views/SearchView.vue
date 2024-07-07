ce4c262a4a796a75f291ad28a5e72760f27c535b
Patryk Rybak <rybakpatryk17@gmail.com>
2024-07-08 00:02:50 +0200

    small css change

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -140,13 +140,16 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 main {
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 15px;
+}
+.top-bar {
+  width: 100%;
 }
 .navigation {
   width: 100%;
